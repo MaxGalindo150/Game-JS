4837a9ac644af0aaf57ad6a7817eff156b6f4791
Maximiliano <maximilianogalindo7@gmail.com>
2023-06-24 19:01:21 -0600

    losing

--- game.js
+++ game.js
@@ -9,6 +9,11 @@ const btnDown = document.querySelector('#down');
 
 let canvasSize;
 let elementsSize;
+let level = 0;
+let initialPlayerPosX;
+let initialPlayerPosY;
+
+
 
 const playerPosition = {
     x: undefined,
@@ -47,7 +52,13 @@ function startGame() {
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
   
-    const map = maps[0];
+    const map = maps[level];
+    
+    if (!map){
+      gameWin();
+      return;
+    }
+
     const mapRows = map.trim().split('\n');
     const mapRowCols = mapRows.map(row => row.trim().split(''));
     //console.log({map, mapRows, mapRowCols});
@@ -62,10 +73,11 @@ function startGame() {
         const posY = elementsSize * (rowI + 1);
 
         if (col == 'O') {
+            initialPlayerPosX = posX;
+            initialPlayerPosY= posY;
             if (!playerPosition.x && !playerPosition.y){
                 playerPosition.x = posX;
                 playerPosition.y = posY;
-                console.log({playerPosition});
             }
         } else if (col == 'I') {
             giftPosition.x = posX;
@@ -90,21 +102,36 @@ function movePlayer() {
     const giftCollision = giftCollisionX && giftCollisionY;
 
     if (giftCollision) {
-        console.log('Next level')
+        levelWin();
     }
 
     const enemyCollision = enemyPositions.find( enemy => {
-        const enemyCollisionX = enemy.x == playerPosition.x;
-        const enemyCollisionY = enemy.y == playerPosition.y;
+        const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+        const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
         return enemyCollisionX && enemyCollisionY
     });
     
     if (enemyCollision) {
-        console.log('ure a fking loser!!!!!')
+        lostLevel();
     }
 
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
-  }
+}
+
+function levelWin() {
+  console.log('Next level')
+  level += 1
+  startGame();
+}
+
+function gameWin() {
+  console.log('You won!!!')
+}
+
+function lostLevel() {
+  playerPosition.x = initialPlayerPosX;
+  playerPosition.y = initialPlayerPosY;
+}
 
 window.addEventListener('keydown', moveByKeys)
 btnUp.addEventListener('click', moveUp);
@@ -132,9 +159,6 @@ function moveUp() {
   }
   function moveLeft() {
     console.log('Left');
-    console.log(playerPosition.x) 
-    console.log(elementsSize)
-    console.log(playerPosition.x - elementsSize)
     if ((playerPosition.x.toFixed(5) - elementsSize.toFixed(5)) < elementsSize) {
       console.log('OUT');
     } else {
